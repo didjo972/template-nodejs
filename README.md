33c6d9e264444685701d646637a8126104f97072
U808266 <dimitri.marierose@mobi.ch>
2020-01-21 17:00:59 +0100

    Second commit

---
+++ README.md
@@ -0,0 +1,16 @@
+**helmet**
+Help us to secure our application by setting various HTTP headers
+**cors**
+Enable cross-origin Requests
+**body-parser**
+Parses the client’s request from json into javascript objects
+**jsonwebtoken**
+Will handle the jwt operations for us
+**bcryptjs**
+Help us to hash user passwords
+**typeorm**
+The ORM we are going to use to manipulate database
+**reflect-metadata**
+allow some annotations features used with TypeORM
+**class-validator**
+A validation package that works really well with TypeORM
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import express, {Request, Response} from "express";
-export const app = express();
+import { createConnection } from "typeorm";
+import express from "express";
+import * as bodyParser from "body-parser";
+import helmet from "helmet";
+import cors from "cors";
+import * as Routes from "./routes";
+
+// Connectes to the database -> then start the express app
+createConnection()
+    .then(async connection => {
+      // Create a new express application instance
+        const app = express();
+
+        // Call middlewares
+        app.use(cors());
+        app.use(helmet());
+        app.use(bodyParser.json());
+
+        //Set all routes from routes folder
+        // @ts-ignore
+            app.use("/", Routes);
+    })
+    .catch(err => console.error(err));
 
 // define a route handler for the default home page
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello world!" });
-});
+// app.get("/", (req: Request, res: Response) => {
+//   res.json({ message: "Hello world!" });
+// });
